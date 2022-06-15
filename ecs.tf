d0b6a09e2b7dc64d4f027dffb580cf9b20dba9c3
penghao <756821993@qq.com>
2022-06-15 15:01:25 +0800

    On branch master  Changes to be committed: 	modified:   ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,6 +1,6 @@
 provider "alicloud" {
-  access_key = ""
-  secret_key = ""
+  access_key = "LTAI5tHWdPtAMWnCC9SQmuoe"
+  secret_key = "bWSRZqkOwubHUsM1TBUY11LZF6BnNw"
   region     = "cn-hangzhou"
 }
 
